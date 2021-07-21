67118d21e3ecdb4c710074561972e6eb27a6f61b
Muz4k Victory <muz4k.oo@gmail.com>
2021-07-21 21:33:47 +0300

    fix hosts & cs

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,11 +18,11 @@ provider "digitalocean" {
   token = var.do_token
 }
 variable "datadog_api_key" {
-  type        = string
+  type = string
 }
 
 variable "datadog_app_key" {
-  type        = string
+  type = string
 }
 
 provider "datadog" {
@@ -35,39 +35,40 @@ data "digitalocean_ssh_key" "victory" {
 }
 
 resource "digitalocean_droplet" "servers" {
-  count              = 2
-  image              = "docker-20-04"
-  name               = "project-${count.index + 1}"
-  region             = "ams3"
-  size               = "s-1vcpu-1gb"
-  ssh_keys = [data.digitalocean_ssh_key.victory.id]
+  count = 2
+  image = "docker-20-04"
+  name = "project-${count.index + 1}"
+  region = "ams3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.victory.id]
 }
 
 resource "digitalocean_loadbalancer" "project" {
-  name   = "project"
+  name = "project"
   region = "ams3"
 
   forwarding_rule {
-    entry_port     = 80
+    entry_port = 80
     entry_protocol = "http"
 
-    target_port     = 1337
+    target_port = 1337
     target_protocol = "http"
   }
 
   forwarding_rule {
-    entry_port     = 443
+    entry_port = 443
     entry_protocol = "https"
 
-    target_port     = 1337
+    target_port = 1337
     target_protocol = "http"
 
     certificate_name = digitalocean_certificate.project.name
   }
 
   healthcheck {
-    port     = 1337
-    path     = "/"
+    port = 1337
+    path = "/"
     protocol = "http"
   }
 
@@ -75,14 +76,15 @@ resource "digitalocean_loadbalancer" "project" {
 }
 
 resource "digitalocean_domain" "default" {
-  name       = "project.gitpushforce.club"
+  name = "project.gitpushforce.club"
   ip_address = digitalocean_loadbalancer.project.ip
 }
 
 resource "digitalocean_certificate" "project" {
-  name    = "project-certificate"
-  type    = "lets_encrypt"
-  domains = [ "project.gitpushforce.club" ]
+  name = "project-certificate"
+  type = "lets_encrypt"
+  domains = [
+    "project.gitpushforce.club"]
 }
 
 output "webserver_ips" {
@@ -91,22 +93,22 @@ output "webserver_ips" {
 
 resource "digitalocean_database_db" "database" {
   cluster_id = digitalocean_database_cluster.postgres.id
-  name       = "strapi"
+  name = "strapi"
 }
 
 resource "digitalocean_database_cluster" "postgres" {
-  name       = "postgres-cluster"
-  engine     = "pg"
-  version    = "11"
-  size       = "db-s-1vcpu-1gb"
-  region     = "ams3"
+  name = "postgres-cluster"
+  engine = "pg"
+  version = "11"
+  size = "db-s-1vcpu-1gb"
+  region = "ams3"
   node_count = 1
 }
 
 resource "datadog_monitor" "healthcheck" {
-  name               =  "Alert ! {{ host.name }}"
-  type               = "service check"
-  message            = " @muz4k.oo@gmail.com"
+  name = "Alert ! {{ host.name }}"
+  type = "service check"
+  message = " @muz4k.oo@gmail.com"
 
   query = "\"http.can_connect\".over(\"instance:app_health_check\",\"url:http://localhost:1337\").by(\"host\",\"instance\",\"url\").last(2).count_by_status()"
 }
