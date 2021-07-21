7720778ea2efc8da8859a9a34f9ce565d2e8db4f
Muz4k Victory <muz4k.oo@gmail.com>
2021-07-21 16:59:19 +0300

    update ports & created terraform database

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -36,7 +36,7 @@ resource "digitalocean_loadbalancer" "project" {
     entry_port     = 80
     entry_protocol = "http"
 
-    target_port     = 5000
+    target_port     = 1337
     target_protocol = "http"
   }
 
@@ -44,7 +44,7 @@ resource "digitalocean_loadbalancer" "project" {
     entry_port     = 443
     entry_protocol = "https"
 
-    target_port     = 5000
+    target_port     = 1337
     target_protocol = "http"
 
     certificate_name = digitalocean_certificate.project.name
@@ -72,5 +72,19 @@ resource "digitalocean_certificate" "project" {
 
 output "webserver_ips" {
   value = digitalocean_droplet.servers.*.ipv4_address
+}
+
+resource "digitalocean_database_db" "database" {
+  cluster_id = digitalocean_database_cluster.postgres.id
+  name       = "strapi"
+}
+
+resource "digitalocean_database_cluster" "postgres" {
+  name       = "postgres-cluster"
+  engine     = "pg"
+  version    = "11"
+  size       = "db-s-1vcpu-1gb"
+  region     = "ams3"
+  node_count = 1
 }
 
